8312a07a74bab646658cce9a455f9d96a5235356
Doga bedriye Bayraktar <dogabayraktar@re-guest-195-169-43-111.wireless.uva.nl>
2021-09-29 18:53:10 +0200

    selected columns for datacleaning

---
+++ src/data_cleaning.R
@@ -0,0 +1,96 @@
+
+
+#---selecting columns---#
+#---install.packages("dplyr")---#
+rm(list = ls())
+
+library(dplyr)
+setwd("/Users/dogabayraktar/Desktop/DPREP/SickAirbnbPricesAcrossNetherlands/src")
+
+#---listings-12.20 dataset---#
+df12_20_unclean <- read.csv("listings-12.20.csv")
+df12_20<-df12_20_unclean%>% select(
+  name,
+  host_id,
+  neighbourhood,
+  room_type,
+  price,
+  number_of_reviews,
+  last_review,
+  reviews_per_month
+)
+
+#---listings-01.21 dataset---#
+
+df01_21_unclean <- read.csv("listings-01.21.csv")
+df01_21<-df01_21_unclean%>% select(
+  name,
+  host_id,
+  neighbourhood,
+  room_type,
+  price,
+  number_of_reviews,
+  last_review,
+  reviews_per_month
+)
+
+#---listings-02.21 dataset---#
+
+df02_21_unclean <- read.csv("listings-02.21.csv")
+df02_21<-df02_21_unclean%>% select(
+  name,
+  host_id,
+  neighbourhood,
+  room_type,
+  price,
+  number_of_reviews,
+  last_review,
+  reviews_per_month
+)
+
+#---listings-03.21 dataset---#
+
+df03_21_unclean <- read.csv("listings-03.21.csv")
+df03_21<-df03_21_unclean%>% select(
+  name,
+  host_id,
+  neighbourhood,
+  room_type,
+  price,
+  number_of_reviews,
+  last_review,
+  reviews_per_month
+)
+
+#---listings-04.21 dataset---#
+
+df04_21_unclean <- read.csv("listings-04.21.csv")
+df04_21<-df04_21_unclean%>% select(
+  name,
+  host_id,
+  neighbourhood,
+  room_type,
+  price,
+  number_of_reviews,
+  last_review,
+  reviews_per_month
+)
+
+#---listings-05.21 dataset---#
+
+df05_21_unclean <- read.csv("listings-05.21.csv")
+df05_21<-df05_21_unclean%>% select(
+  name,
+  host_id,
+  neighbourhood,
+  room_type,
+  price,
+  number_of_reviews,
+  last_review,
+  reviews_per_month
+)
+
+
+
+
+
