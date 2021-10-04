15729caf44dd289b8bda4f9b5302a420d7181326
Sam Messaoudi <89806126+SamMes98@users.noreply.github.com>
2021-10-04 18:18:32 +0200

    Setup for cleaning which resulted in the first merged data set

--- src/data-prep/data_cleaning.R
+++ src/data-prep/data_cleaning.R
@@ -1,18 +1,15 @@
-
-
-#---selecting columns---#
-#---install.packages("dplyr")---#
-rm(list = ls())
+######################
+#### CLEAN DATA ######
+######################
 
 library(dplyr)
-setwd("/Users/dogabayraktar/Desktop/DPREP/SickAirbnbPricesAcrossNetherlands/src")
-
+library(stringr)
 #---listings-12.20 dataset---#
-df12_20_unclean <- read.csv("listings-12.20.csv")
+df12_20_unclean <- read.csv("../../data/listings-12.20.csv")
 df12_20<-df12_20_unclean%>% select(
-  name,
+  id,
   host_id,
-  neighbourhood,
+  neighbourhood_cleansed,
   room_type,
   price,
   number_of_reviews,
@@ -20,7 +17,7 @@ df12_20<-df12_20_unclean%>% select(
   reviews_per_month
 )
 
-# Cleaning the price column and transforming it to numeric
+# Cleaning the price column and transforming to numeric
 df12_20 <- df12_20 %>%  
   mutate(price = str_remove_all(price, fixed("$"))) %>% 
   mutate(price = str_remove_all(string = price, pattern = fixed(","))) %>% 
@@ -30,19 +27,41 @@ df12_20 <- df12_20 %>%
 df12_20 <- df12_20 %>%  
   mutate(last_review = as.Date(last_review))
 
-#---listings-01.21 dataset---#
+# No duplicated rows
+filter(df12_20, duplicated(df12_20)) 
 
-df01_21_unclean <- read.csv("listings-01.21.csv")
+#---listings-01.21 dataset---#
+df01_21_unclean <- read.csv("../../data/listings-01.21.csv")
 df01_21<-df01_21_unclean%>% select(
-  name,
+  id,
   host_id,
-  neighbourhood,
+  neighbourhood_cleansed,
   room_type,
   price,
   number_of_reviews,
   last_review,
   reviews_per_month
 )
+
+# Cleaning the price column and transforming to numeric
+df01_21 <- df01_21 %>%  
+  mutate(price = str_remove_all(price, fixed("$"))) %>% 
+  mutate(price = str_remove_all(string = price, pattern = fixed(","))) %>% 
+  mutate(price = as.numeric(x = price))
+
+# last_review as a date variable
+df01_21 <- df01_21 %>%  
+  mutate(last_review = as.Date(last_review))
+
+# No duplicated rows
+filter(df01_21, duplicated(df01_21)) 
+
+# Merging both data sets
+df_merged12.20_01.21 <- df12_20 %>% 
+  inner_join(df01_21, by = c("id" = "id"))
+
+# Saving the dataset
+write.csv(df_merged12.20_01.21, "../../gen/temp/comp12.20-01.21.csv")
 
 #---listings-02.21 dataset---#
 
