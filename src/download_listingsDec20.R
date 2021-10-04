aa77dd769fd892a87283097fb26319997e050783
Sam Messaoudi <89806126+SamMes98@users.noreply.github.com>
2021-10-04 11:51:18 +0200

    Added an alternative way to download and save data

--- src/download_listingsDec20.R
+++ src/download_listingsDec20.R
@@ -10,4 +10,18 @@ drive_download(
   path = out_file,
   overwrite = TRUE)
 
-  
+ # Alternative wat to download data and save it 
+######################
+### DOWNLOAD DATA ####
+######################
+
+library(dplyr)
+library(reshape2)
+
+download_data <- function(url, filename){
+  download.file(url = url, destfile = paste0("../../data/", filename, ".csv"))
+}
+
+url_listings12.20 <- "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2020-12-12/data/listings.csv"
+
+download_data(url_listings12.20, "listings-12.20")
