c80fdf77d6f04b77b3baff8f992e9b41aca3e3c8
Doga bedriye Bayraktar <dogabayraktar@re-guest-195-169-174-50.wireless.uva.nl>
2021-10-11 12:13:28 +0200

    minor update spelling

--- src/analysis/Analysis.R
+++ src/analysis/Analysis.R
@@ -1,5 +1,5 @@
 
-setwd("/Users/dogabayraktar/Desktop/DPREP/SickAirbnbPricesAcrossNetherlands/gen/temp")
+
 # load in the complete price comparion between all six data set
 price_comparison <- read.csv('../../gen/temp/complete_price_comparison.csv')
 
