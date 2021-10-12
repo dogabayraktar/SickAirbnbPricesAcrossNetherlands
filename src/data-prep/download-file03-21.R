46c8c9722a7c4b120d8268b2f910fedc6fa4385d
Sam <sam.messaoudi@hotmail.nl>
2021-10-12 10:40:51 +0200

    fixed the dprep makefile for data download

---
+++ src/data-prep/download-file03-21.R
@@ -0,0 +1,5 @@
+# The listing
+url_listings03.21 <- "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-03-04/data/listings.csv.gz"
+
+# downloading the file and saving it in the data folder
+download.file(url_listings03.21, "../../data/listings-03.21.csv.gz")
---
+++ src/data-prep/download-file05-21.R
@@ -0,0 +1,5 @@
+# The listing
+url_listings05.21 <- "http://data.insideairbnb.com/the-netherlands/north-holland/amsterdam/2021-05-19/data/listings.csv.gz"
+
+# downloading the file and saving it in the data folder
+download.file(url_listings05.21, "../../data/listings-05.21.csv.gz")
