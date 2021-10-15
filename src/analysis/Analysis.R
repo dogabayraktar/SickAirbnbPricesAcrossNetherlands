081cb8e56de440df6981ad2be12987c2af3d6209
Doga bedriye Bayraktar <dogabayraktar@Dogas-MacBook-Pro.local>
2021-10-15 19:59:46 +0200

    created Rmarkdown with introduction and knittings

--- src/analysis/Analysis.R
+++ src/analysis/Analysis.R
@@ -30,7 +30,8 @@ only_price_district <- price_comparison%>%
 #creating a new column with the price difference
 only_price_district$price_difference<-((only_price_district$price_05.21)-(only_price_district$price_12.20))
 
-
+#saving the further coerced data-set as only price district
+write.csv(only_price_district, "../../gen/temp/only_price_district.csv")
 
 #---ANOVA Analysis---#
 
@@ -46,7 +47,7 @@ group_by(only_price_district,neighbourhood_cleansed)%>%
 #---Visualizing data---#
 
 
-# Mean plots- Plot district by price difference,add error bars: mean_se, (other values include: mean_sd, mean_ci, median_iqr, ....)
+# Mean plots- Plot district by price difference,add error bars
 meanplot_pricedif_district <- ggline(only_price_district, x = "neighbourhood_cleansed", y = "price_difference",
        add = c("mean_se", "jitter"), 
        ylab = "District", xlab = "Price difference")
@@ -73,12 +74,12 @@ only_price_district %>% count(neighbourhood_cleansed)
 #for normality
 ks.test(only_price_district$price_difference, "pnorm", mean=mean(only_price_district$price_difference), sd=sd(only_price_district$price_difference))
 
-#computing ANOVA
+#Computing ANOVA
 only_price_district.aov<-aov(price_difference~neighbourhood_cleansed, data = only_price_district)
 summary(only_price_district.aov)
 
 
-#plot homogenity of variance
+#Plot homogenity of variance
 plot(only_price_district.aov, 1)
 #pairwise comparison for differences among districts
 TukeyHSD(only_price_district.aov) #diff: difference between means of the two groups
