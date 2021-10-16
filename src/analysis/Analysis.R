22c28e2f076fac27769a7831a4d786c31b86903c
Sam <sam.messaoudi@hotmail.nl>
2021-10-16 12:34:54 +0200

    used knit on the Rmarkdown and made some changes to make it run

--- src/analysis/Analysis.R
+++ src/analysis/Analysis.R
@@ -30,6 +30,8 @@ only_price_district <- price_comparison%>%
 #creating a new column with the price difference
 only_price_district$price_difference<-((only_price_district$price_05.21)-(only_price_district$price_12.20))
 
+#writing the CSV file
+write.csv(only_price_district, "../../gen/temp/only_price_district.csv")
 
 #---ANOVA Analysis---#
 
