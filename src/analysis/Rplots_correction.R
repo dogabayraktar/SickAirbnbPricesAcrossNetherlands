e81418b16fe41adb34685c84a52062bf0f13d59f
Sam <sam.messaoudi@hotmail.nl>
2021-10-17 13:13:28 +0200

    added extra explanation why this script is necessary

--- src/analysis/Rplots_correction.R
+++ src/analysis/Rplots_correction.R
@@ -1,2 +1,3 @@
-# correction to remove unwanted Rplots.pdf that is created by the makefile when executed with the command line
+# correction to remove unwanted Rplots.pdf that is created by the makefile when executed with the command line.
+# This is a common problem but it is a bit complicated to solve: https://stackoverflow.com/questions/17348359/how-to-stop-r-from-creating-empty-rplots-pdf-file-when-using-ggsave-and-rscript
 unlink("Rplots.pdf", force = TRUE)
